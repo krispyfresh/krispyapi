c6d763346f7f388268dfbea7343d24db58b0098a
Chris Parsons <chris.parsons@rackspace.com>
2013-03-27 20:59:48 -0500

    cleaned up the code and took out unnecessary waits

--- challenge1.php
+++ challenge1.php
@@ -12,18 +12,22 @@ ini_set('include_path','./lib:'.ini_get(include_path));
 //set the requested OS and memory for the new Cloud Server
 $OS = 'CentOS 6.3';
 $MEMORY = 512;
-$SERVERNAME = "web";
+$SERVERNAME = "db";
 
 //include the rackspace library
 require('rackspace.php');
 
+//read the ini file and store it in $ini as an array
+//ini file is at .rackspace_cloud_credentials and contains:
+//[authentication]
+//username: $your_username
+//apikey: $your_apikey
 $ini = parse_ini_file(".rackspace_cloud_credentials", TRUE);
 
 //get an auth token by passing the username and api key to the auth server
-define('AUTHURL', RACKSPACE_US);
 $auth = array('username' => $ini['authentication']['username'],
               'apiKey' => $ini['authentication']['apikey']);
-$rsconnect = new Rackspace(AUTHURL, $auth);
+$rsconnect = new Rackspace(RACKSPACE_US, $auth);
 $cloudservers = $rsconnect -> Compute('cloudServersOpenStack', 'DFW');
 
 //go through the list of flavors to find the right one
@@ -44,18 +48,17 @@ while($i = $imagelist -> Next())
         $RS_IMAGE = $i;
 }
 
-//create the server
+//set the cloud server properties
 $serverinfo = array('image' => $RS_IMAGE,
                     'flavor' => $RS_FLAVOR);
 $newserver = $cloudservers -> Server();
 
+//create each cloud server
 for($a = 1; $a <=3; $a++)
 {
-    $newserver -> name = $SERVERNAME . (string)$a;
+    $newserver -> name = $SERVERNAME . (string)$a;  //appends the incremental number to the end of $SERVERNAME
     $newserver -> Create($serverinfo);
-    print("Building your $OS server named ".$newserver -> name."...\n");
-    $newserver->WaitFor("ACTIVE", 600);
-    print($newserver -> name." build is complete.\n");    
+    print("Building your $OS server named ".$newserver -> name."...\n");  
 }
 
 exit(0);
