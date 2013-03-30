13f43fda77ad0e9b9bb13ef9d0b1981dd687518d
Chris Parsons <chris.parsons@rackspace.com>
2013-03-29 19:26:07 -0500

    got challenge 3 working

--- challenge3.php
+++ challenge3.php
@@ -7,8 +7,8 @@ namespace OpenCloud;
 //include the lib directory in the working path
 ini_set('include_path','./lib:'.ini_get(include_path));
 
-$DIRECTORY = $argv[1];  //This is the name of the Cloud Server you want to clone
-$CONTAINER = $argv[2]; //Images are named $SERVERNAME.$IMAGEDESC
+$DIRECTORY = $argv[1];  //Upload files from this directory
+$CONTAINER = $argv[2]; //Upload files to this Cloud Files container
 
 //include the rackspace library
 require('rackspace.php');
@@ -24,5 +24,48 @@ $ini = parse_ini_file(".rackspace_cloud_credentials", TRUE);
 $auth = array('username' => $ini['authentication']['username'],
               'apiKey' => $ini['authentication']['apikey']);
 $rsconnect = new Rackspace(RACKSPACE_US, $auth);
+
+//create a handle to Cloud Files
+$cloudfiles = $rsconnect -> ObjectStore('cloudFiles', 'DFW');
+
+//check to see if the container exists
+$containerlist = $cloudfiles -> ContainerList();
+$containerfound = false;
+while($c = $containerlist -> Next())
+{
+    //if the container already exists, we don't have to do anything but save a handle to it
+    if($c -> name == $CONTAINER)
+    {
+        $containerfound = true;
+        $container = $c;
+        print("Found container\n");
+    }
+}
+
+//if the container doesn't exist, create it
+if($containerfound == false)
+{
+    $container = $cloudfiles -> Container();
+    $container -> Create(array('name' => $CONTAINER));
+    print("Created container $CONTAINER\n");
+}
+
+
+$dir = opendir($DIRECTORY);
+$file = readdir($dir);
+while($file = readdir($dir))
+{
+    if(!($file === ".") && !($file === ".."))
+    {
+        print("Uploading $file to Cloud Files container $CONTAINER ");
+        $newobject = $container -> DataObject();
+        $newobject -> Create(array('name' => $file),__FILE__);
+        print("of size ".$newobject -> size."\n");
+    }
+}
+
+
+
+
 
 ?>
