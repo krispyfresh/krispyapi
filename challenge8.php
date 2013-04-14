83fe81f305a7c3d99c6068f6d910025016ec407c
Chris Parsons <chris.parsons@rackspace.com>
2013-04-14 12:52:57 -0500

    cleaned up comments and added wait loops

--- challenge8.php
+++ challenge8.php
@@ -3,6 +3,8 @@
 namespace OpenCloud;
 
 // Author: Chris Parsons
+// this program takes a static webpage, uploads it to and serves it out of cloud files,
+// CDN enables it, and creates a new CNAME record for it.
 
 // include the lib directory in the working path
 ini_set('include_path','./lib:'.ini_get(include_path));
@@ -10,17 +12,26 @@ ini_set('include_path','./lib:'.ini_get(include_path));
 // include the rackspace library
 require('rackspace.php');
 
+// check that we have the right number of arguments
+if(sizeof($argv) != 3)
+{
+    print("Incorrect number of arguments!\n");
+    print("Usage: challenge8.php <local path to index file> <FQDN of new website>\n");
+    exit();
+}
+
 $INDEX = $argv[1];  // the file you want to publish on Cloud Files, must be in the program folder
-$CNAME = $argv[2]; // the CNAME pointing to the CDN URL
+$FQDN = $argv[2]; // the FQDN of the CNAME record that will point to the CDN URL of the website
 $CONTAINER = "Website_Files"; // the name of the Cloud Files container that will be created
 $EMAIL = "chris.parsons@rackspace.com"; // email to use for the new DNS zone
+$DC = 'DFW'; // DC to create all this stuff in
 
 // read the ini file and store it in $ini as an array
 // ini file is at .rackspace_cloud_credentials and contains:
 // [authentication]
 // username: $your_username
 // apikey: $your_apikey
-$ini = parse_ini_file(".rackspace_cloud_credentials", TRUE);
+$ini = parse_ini_file('.rackspace_cloud_credentials', TRUE);
 
 // get an auth token by passing the username and api key to the auth server
 $auth = array('username' => $ini['authentication']['username'],
@@ -28,7 +39,7 @@ $auth = array('username' => $ini['authentication']['username'],
 $rsconnect = new Rackspace(RACKSPACE_US, $auth);
 
 // create a handle to Cloud Files
-$cloudfiles = $rsconnect -> ObjectStore('cloudFiles', 'DFW');
+$cloudfiles = $rsconnect -> ObjectStore('cloudFiles', $DC);
 
 // create the container
 $container = $cloudfiles -> Container();
@@ -52,19 +63,19 @@ $dns = $rsconnect -> DNS();
 
 // get the actual domain name
 // if desired CNAME is test.krispy.com, this will return krispy.com
-$domainname = substr($CNAME, strpos($CNAME, '.') + 1); 
+$domainname = substr($FQDN, strpos($FQDN, '.') + 1); 
 
 $domain = $dns -> Domain(array('name' => $domainname,
                                'emailAddress' => $EMAIL,
                                'ttl' => 28800));
 
 $record = $domain -> Record(array('type' => 'CNAME',
-                                  'name' => $CNAME,
+                                  'name' => $FQDN,
                                   'ttl' => 28800,
                                   'data' => $indexfile -> CDNUrl()));
 $domain -> AddRecord($record);
 $response = $domain -> Create();
 $response -> WaitFor("COMPLETED",10);
-print("Created DNS zone for $domainname with CNAME record $CNAME...\n");
+print("Created DNS zone for $domainname with CNAME record $FQDN...\n");
 
 ?>
--- challenge9.php
+++ challenge9.php
@@ -3,74 +3,110 @@
 namespace OpenCloud;
 
 // Author: Chris Parsons
+// this program takes an FQDN, OS name, and amount of memory in MB, and creates a
+// new cloud server with those specs, then creates a DNS record pointing to the new server
+// $OS should be something like 'CentOS 6.3', 'Windows 2008 Standard', etc.
+// $MEMORY should be in MB, so for a 4 GB server, enter 4096
 
-//include the lib directory in the working path
+// include the lib directory in the working path
 ini_set('include_path','./lib:'.ini_get(include_path));
 
-//include the rackspace library
+// include the rackspace library
 require('rackspace.php');
+
+// check that we have the right number of arguments
+if(sizeof($argv) != 4)
+{
+    print("Incorrect number of arguments!\n");
+    print("Usage: challenge9.php <FQDN of new server> <OS of new server> <memory of new server in MB>\n");
+    exit();
+}
 
 $FQDN = $argv[1];
 $OS = $argv[2];
 $MEMORY = $argv[3];
 $EMAIL = "chris.parsons@rackspace.com"; //email to use for the new DNS zone
 
-//read the ini file and store it in $ini as an array
-//ini file is at .rackspace_cloud_credentials and contains:
-//[authentication]
-//username: $your_username
-//apikey: $your_apikey
+// read the ini file and store it in $ini as an array
+// ini file is at .rackspace_cloud_credentials and contains:
+// [authentication]
+// username: $your_username
+// apikey: $your_apikey
 $ini = parse_ini_file(".rackspace_cloud_credentials", TRUE);
 
-//get an auth token by passing the username and api key to the auth server
+// get an auth token by passing the username and api key to the auth server
 $auth = array('username' => $ini['authentication']['username'],
               'apiKey' => $ini['authentication']['apikey']);
 $rsconnect = new Rackspace(RACKSPACE_US, $auth);
 
-//create a handle to Cloud Servers
+// create a handle to Cloud Servers
 $cloudservers = $rsconnect -> Compute('cloudServersOpenStack', 'DFW');
 
-//go through the list of flavors to find the right one
+// go through the list of flavors to find the right one
 $flavorlist = $cloudservers -> FlavorList();
+$flavorfound = false;
 while($f = $flavorlist -> Next())
 {
     if($f -> ram == $MEMORY)
+    {
         $flavor = $f;
+        $flavorfound = true;
+    }
 }
 
-//go through the list of images to find the right one
+// go through the list of images to find the right one
 $imagelist = $cloudservers -> ImageList();
+$imagefound = false;
 while($i = $imagelist -> Next())
 {
     if($i -> name == $OS)
+    {
         $image = $i;
+        $imagefound = true;
+    }
 }
 
-$server = $cloudservers -> Server();
-$server -> Create(array('name' => $FQDN,
-                                     'image' => $image,
-                                     'flavor' => $flavor));
-print("Creating Cloud Server $FQDN...\n");
-$server -> WaitFor("COMPLETE",300);
+if($imagefound && $flavorfound)
+{
+    $server = $cloudservers -> Server();
+    $server -> Create(array('name' => $FQDN,
+                            'image' => $image,
+                            'flavor' => $flavor));
+    print("Creating Cloud Server $FQDN...\n");  
+    while($server -> status != 'ACTIVE')
+    {
+        $server = $cloudservers -> Server($server -> id); // refresh the server info so we can check the current status
+        sleep(15);
+    }
+    print("Server creation complete!\n");
+    
+    // create a handle to Cloud DNS
+    $dns = $rsconnect -> DNS();
 
-//create a handle to Cloud DNS
-$dns = $rsconnect -> DNS();
+    // get the actual domain name
+    // if new DNS record is for test.krispy.com, this will return krispy.com
+    $domainname = substr($FQDN, strpos($FQDN, '.') + 1);
+    
+    $domain = $dns -> Domain(array('name' => $domainname,
+                                   'emailAddress' => $EMAIL,
+                                   'ttl' => 28800));
 
-//get the actual domain name
-//if new DNS record is for test.krispy.com, this will return krispy.com
-$domainname = substr($FQDN, strpos($FQDN, '.') + 1);
+    $record = $domain -> Record(array('type' => 'A',
+                                      'name' => $FQDN,
+                                      'ttl' => 28800,
+                                      'data' => $server -> ip(4)));
+    $domain -> AddRecord($record);
+    $response = $domain -> Create();
+    print("Creating A record for $FQDN in DNS zone $domainname...\n");
+}
 
-$domain = $dns -> Domain(array('name' => $domainname,
-                               'emailAddress' => $EMAIL,
-                               'ttl' => 28800));
-
-$record = $domain -> Record(array('type' => 'A',
-                                  'name' => $FQDN,
-                                  'ttl' => 28800,
-                                  'data' => $server -> ip(4)));
-$domain -> AddRecord($record);
-$response = $domain -> Create();
-print("Creating A record for $FQDN in DNS zone $domainname...\n");
-$response -> WaitFor("COMPLETED",10);
+else
+{
+    
+    if(!$imagefound)
+        print("Image not found!\n");
+    if(!$flavorfound)
+        print("Flavor not found!\n");
+}
 
 ?>
